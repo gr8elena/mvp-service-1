141a1acbb9f25dc6a3ee3072eb3bd583ef87ffdc
unknown <shulevskaelena@gmail.com>
2025-11-13 23:26:13 +0100

    initial

---
+++ src/main/java/com/example/mvp_service_1/MvpService1Application.java
@@ -0,0 +1,13 @@
+package com.example.mvp_service_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvp_service_1/MvpService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvp_service_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
